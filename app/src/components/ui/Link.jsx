65e40b43b5f5e438466bc2c3125d40a69bddacbe
Hadiya Amber <hadiya.amber@kanini.com>
2026-01-14 16:15:01 +0530

    login service few changes

--- app/src/components/ui/Link.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Link({ children, href, ...props }) {
-  return (
-    <a className="ui-link" href={href} {...props}>
-      {children}
-    </a>
-  )
-}
